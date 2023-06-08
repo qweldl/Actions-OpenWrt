bd4bc28f9e5207024ce5b1bd8225ec782aaef619
markvlenvision <46559300+markvlenvision@users.noreply.github.com>
2023-06-08 15:20:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman' >>feeds.conf.default
-echo 'src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
-echo 'src-git openwrtPackages https://github.com/markvlenvision/openwrt-packages.git' >>feeds.conf.default
+echo 'src-git destan19 https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
 
