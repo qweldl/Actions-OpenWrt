e92b58d72904066b7fdbefd7eca03512ee010d1d
zhanghanqing <zhanghanqing@mail.com>
2023-08-02 13:01:49 +0800

    update code

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,18 +17,16 @@
 
 # Add a feed source
 
-echo '#src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-echo '#src-git gngpp https://github.com/gngpp/xunlei' >>feeds.conf.default
+git clone https://github.com/fw876/helloworld.git package/helloworld
+git -C package/helloworld reset --hard b7451f4dc495d74fe5dc5a4597d659f9515dd049
 
-echo 'src-git kenzok8 https://github.com/markvlenvision/small-package' >>feeds.conf.default
-echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman' >>feeds.conf.default
-echo 'src-git destan19 https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/AdguardTeam/AdGuardHome package/adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/appadguardhome
+git clone https://github.com/sirpdboy/luci-app-eqosplus package/eqosplus
+git clone https://github.com/gngpp/xunlei package/xunlei
 
-
-git clone https://github.com/fw876/helloworld.git package/lean/helloworld
-git -C package/lean/helloworld reset --hard b7451f4dc495d74fe5dc5a4597d659f9515dd049
-
-git clone https://github.com/gngpp/xunlei package/lean/xunlei
-
+#git clone https://github.com/lisaac/luci-app-dockerman package/dockerman
+#git clone https://github.com/kenzok8/small-package package/kenzok8
 
 #https://github.com/openwrt/luci/tree/master/applications/luci-app-acl
--- diy-part2.sh
+++ diy-part2.sh
@@ -21,10 +21,12 @@
 
 ##修改默认主机名
 #sed -i "s/hostname='.*'/hostname='$OWRT_NAME'/g" ./package/base-files/files/bin/config_generate
+
 ##修改默认时区
 #sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
 #sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
-#
+
+
 ##根据源码来修改
 #if [[ $OWRT_URL == *"lede"* ]] ; then
 #  #修改默认时间格式
