6cc685a5e9c0f8ba1feec8528b8b014c7a85cfc6
zhanghanqing <zhanghanqing@mail.com>
2023-08-07 14:57:14 +0800

    update code

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,15 +17,11 @@
 
 # Add a feed source
 
-git clone https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/fw876/helloworld.git package/lean/helloworld
 git -C package/helloworld reset --hard b7451f4dc495d74fe5dc5a4597d659f9515dd049
 
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/AdguardTeam/AdGuardHome package/adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/appadguardhome
-git clone https://github.com/sirpdboy/luci-app-eqosplus package/eqosplus
-git clone https://github.com/gngpp/xunlei package/xunlei
-git clone https://github.com/riverscn/openwrt-iptvhelper.git package/iptvhelper
+git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
+git clone https://github.com/sirpdboy/luci-app-eqosplus package/lean/eqosplus
 
 #git clone https://github.com/lisaac/luci-app-dockerman package/dockerman
 #git clone https://github.com/kenzok8/small-package package/kenzok8
