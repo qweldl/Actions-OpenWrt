7c8dc747acd0eb2a44eaef2b3478438f4af64aa1
zhanghanqing <zhanghanqing@mail.com>
2023-08-13 15:30:14 +0800

    编译原生openwrt

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,15 +19,29 @@
 #echo 'src-git oaf https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
 #echo 'src-git xunlei https://github.com/gngpp/xunlei' >>feeds.conf.default
 
-
-# 下载源码
+#helloworld
 git clone https://github.com/fw876/helloworld.git package/helloworld
 git -C package/helloworld reset --hard b7451f4dc495d74fe5dc5a4597d659f9515dd049
 
-git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+##adguardhome
+#git clone https://github.com/AdguardTeam/AdGuardHome package/adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#
+##OpenAppFilter
+#git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
+#
+##iptv
+#git clone https://github.com/riverscn/openwrt-iptvhelper.git package/iptvhelper
+#
+##eqosplus
+#git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+#
+##dockerman
+#git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+#
+##xunlei
+#git clone https://github.com/gngpp/xunlei package/luci-app-xunlei
 
-git clone https://github.com/AdguardTeam/AdGuardHome package/adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 #git clone https://github.com/lisaac/luci-app-dockerman package/dockerman
 #git clone https://github.com/kenzok8/small-package package/kenzok8
